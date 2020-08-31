5174b199419fa97d9347610e07d68ddcbb7f82f7
JungHun Kim <mnthe@pubg.com>
2020-08-31 22:47:39 +0900

    Add EnvVars to trigger.sh to modify server endpoint
    - FLUSH_CONNTRACK_ENDPOINT: Full Override
    - TARGET_SERVICE: Service Override

--- trigger.sh
+++ trigger.sh
@@ -12,5 +12,20 @@ then
     exit 1
 fi
 
-echo "$TARGET_IP:$TARGET_PORT"
-curl "http://flush-conntrack.$TARGET_NAMESPACE/flush?ip=$TARGET_IP&port=$TARGET_PORT"
+if [ -z $FLUSH_CONNTRACK_ENDPOINT ];
+then
+    if [ -z $TARGET_SERVICE ];
+    then
+        TARGET_SERVICE="flush-conntrack"
+    fi
+    if [ -z $TARGET_NAMESPACE ];
+    then
+        TARGET_NAMESPACE="default"
+    fi
+    FLUSH_CONNTRACK_ENDPOINT="http://$TARGET_SERVICE.$TARGET_NAMESPACE"
+fi
+
+echo "FLUSH_CONNTRACK_ENDPOINT:$FLUSH_CONNTRACK_ENDPOINT"
+echo "TARGET_IP:$TARGET_IP"
+echo "TARGET_PORT:$TARGET_PORT"
+curl "$FLUSH_CONNTRACK_ENDPOINT/flush?ip=$TARGET_IP&port=$TARGET_PORT"
